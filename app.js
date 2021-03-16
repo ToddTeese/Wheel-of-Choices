db152f165bcf9c9073d01ff29d2a070c2334b3d6
Todd <teese.todd@gmail.com>
2021-03-16 22:36:42 +0000

    finish up on the delta for animation

--- app.js
+++ app.js
@@ -1,22 +1,67 @@
 console.log('App connected Bro!');
 
 const wheelHolder = document.querySelector('.wheelHolder');
+const ctx = wheelHolder.getContext('2d');
+const options = {width: 600, height: 600, numberOfSegments: 10}; // allow user input on options
+let colors = ['blue', 'green', 'yellow', 'red', 'cyan', 'seagreen']; // TODO More Colours!
 
-console.log(wheelHolder);
-
-// so, what's the idea of this?
-// it's basically creation of options based on some form of delimiters
-//
-// also, how do I make a segmented circle easily?
-// I think..... canvas?
-// with canvas I can use the radial method and build up segments
-// 
-
-wheelHolder.width = 600;
-wheelHolder.height = 600;
+wheelHolder.width = options.width;
+wheelHolder.height = options.height;
 wheelHolder.style.border = 'solid 1px black';
 
-// yeah this is going to be a job for arcs
+function convertToRadians(degrees) {
+    return (Math.PI/180) * degrees;
+}
 
-// arcTo (x1, y1, x2, y2, radius)
-//
+function drawWheel(ctx, options, colors, offset) {
+    const centerX = options.width / 2;
+    const centerY = options.height / 2;
+    const segmentSize = 360 / options.numberOfSegments;
+
+    for(let i = 0; i < options.numberOfSegments; i++) {
+        let start = (segmentSize * i) + offset;
+        let end = (start + segmentSize);
+        let color = colors[i % colors.length];
+
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.fillStyle = color;
+        ctx.moveTo(centerX, centerY);
+        ctx.arc(centerX, centerY, options.width / 3, convertToRadians(start), convertToRadians(end), false);
+        ctx.moveTo(centerX, centerY);
+        ctx.closePath();
+        ctx.fill();
+    }
+}
+
+
+let offset = 0;
+
+let start;
+let previousTimeStamp;
+
+function step(timestamp) {
+
+    if (start === undefined) {
+        start = timestamp;
+    }
+    const elapsed = timestamp - start;
+
+    let delta = 0;
+    // delta is currentTimeStamp - previousTimeStamp;
+    if(previousTimeStamp) {
+        delta = timestamp - previousTimeStamp;
+    }
+    previousTimeStamp = timestamp;
+
+    drawWheel(ctx, options, colors, offset);
+
+    offset += 0.05 * delta;
+    offset = offset < 360 ? offset :offset % 360;
+  
+    if (elapsed < 6000) { // Stop the animation after 2 seconds
+      window.requestAnimationFrame(step);
+    }
+  }
+
+  window.requestAnimationFrame(step);
