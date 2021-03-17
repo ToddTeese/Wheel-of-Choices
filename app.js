85190875465460b469e1df4eb92f08cc44157dac
Todd <teese.todd@gmail.com>
2021-03-17 22:02:59 +0000

    wheel class to own class - selected segment available

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ console.log('App connected Bro!');
 
 const wheelHolder = document.querySelector('.wheelHolder');
 const ctx = wheelHolder.getContext('2d');
-const options = {width: 600, height: 600, numberOfSegments: 10}; // allow user input on options
-let colors = ['blue', 'green', 'yellow', 'red', 'cyan', 'seagreen']; // TODO More Colours!
+const options = {width: 600, height: 600, numberOfSegments: 3}; // allow user input on options
 
 wheelHolder.width = options.width;
 wheelHolder.height = options.height;
@@ -13,29 +12,31 @@ function convertToRadians(degrees) {
     return (Math.PI/180) * degrees;
 }
 
-function drawWheel(ctx, options, colors, offset) {
-    const centerX = options.width / 2;
-    const centerY = options.height / 2;
-    const segmentSize = 360 / options.numberOfSegments;
+let formInput = document.querySelector('form');
+formInput.addEventListener('submit', event => {
+    event.preventDefault();
+    console.log('Form Event');
 
-    for(let i = 0; i < options.numberOfSegments; i++) {
-        let start = (segmentSize * i) + offset;
-        let end = (start + segmentSize);
-        let color = colors[i % colors.length];
-
-        ctx.beginPath();
-        ctx.strokeStyle = color;
-        ctx.fillStyle = color;
-        ctx.moveTo(centerX, centerY);
-        ctx.arc(centerX, centerY, options.width / 3, convertToRadians(start), convertToRadians(end), false);
-        ctx.moveTo(centerX, centerY);
-        ctx.closePath();
-        ctx.fill();
+    const segments = document.querySelector('#segmentInput').value;
+    console.log(segments);
+    if(segments) {
+        options.numberOfSegments = segments;
+        wheelObj.setNumberOfSegments(options.numberOfSegments);
     }
-}
+})
 
+let outputText = document.querySelector('#outputText');
+
+let wheelObj = new Wheel();
+wheelObj.setNumberOfSegments(options.numberOfSegments);
+wheelObj.centerX = options.width / 2;
+wheelObj.centerY = options.height / 2;
+wheelObj.radius = options.width / 3;
 
 let offset = 0;
+
+wheelObj.draw(ctx, offset);
+console.log({wheel: wheelObj});
 
 let start;
 let previousTimeStamp;
@@ -54,14 +55,16 @@ function step(timestamp) {
     }
     previousTimeStamp = timestamp;
 
-    drawWheel(ctx, options, colors, offset);
+    wheelObj.draw(ctx,offset);
+
+    outputText.innerText = wheelObj.getSelectedSegment(offset);
 
     offset += 0.05 * delta;
     offset = offset < 360 ? offset :offset % 360;
   
-    if (elapsed < 6000) { // Stop the animation after 2 seconds
+    // if (elapsed < 6000) { // Stop the animation after 2 seconds
       window.requestAnimationFrame(step);
-    }
+    // }
   }
 
   window.requestAnimationFrame(step);
---
+++ wheelClass.js
@@ -0,0 +1,71 @@
+class WheelSegment {
+    constructor() {
+        this.startDegree = 0;
+        this.degreeSize = 0;
+        this.endDegree = 0;
+        this.color = 'blue';
+        this.segmentId = 0;
+    }
+
+    setArcDegrees(start, degreeSize) {
+        this.startDegree = start;
+        this.degreeSize = degreeSize;
+        this.endDegree = this.startDegree + degreeSize;
+    }
+
+    draw(ctx, centerX, centerY, radius, offset) {
+        ctx.beginPath();
+        ctx.strokeStyle = this.color;
+        ctx.fillStyle = this.color;
+        ctx.moveTo(centerX, centerY);
+        ctx.arc(centerX, centerY, radius, convertToRadians(this.startDegree + offset), convertToRadians(this.startDegree + offset + this.degreeSize), false);
+        ctx.moveTo(centerX, centerY);
+        ctx.closePath();
+        ctx.fill();
+    }
+}
+
+class Wheel {
+    constructor() {
+        this.segments = [];
+        this.numberOfSegments = 0;
+        this.centerX = 0;
+        this.centerY = 0;
+        this.radius = 0;
+        this.colors = ['blue', 'green'];
+    }
+
+    setNumberOfSegments(value) {
+        this.numberOfSegments = value;
+        this.segmentSize = 360 / this.numberOfSegments;
+
+        for(let i = 0; i < value; i++) {
+            let newSegment = new WheelSegment();
+            newSegment.setArcDegrees((this.segmentSize * i), this.segmentSize);
+            let red = Math.floor(Math.random() * 255);
+            let blue = Math.floor(Math.random() * 255);
+            let green = Math.floor(Math.random() * 255);
+            newSegment.color = `rgb(${red},${blue},${green})`;
+            newSegment.segmentId = i;
+            this.segments.push(newSegment);
+        }
+    }
+
+    draw(ctx, offset) {
+        for(let i = 0; i< this.segments.length; i++) {
+            this.segments[i].draw(ctx, this.centerX, this.centerY, this.radius, offset);
+        }
+    }
+
+    getSelectedSegment(offset) {
+        let selectedSegmentId;
+
+        for(let i = 0; i < this.segments.length; i++) {
+            if(offset > this.segments[i].startDegree && offset < this.segments[i].endDegree) {
+                selectedSegmentId = this.segments[i].segmentId;
+            }
+        }
+
+        return selectedSegmentId;
+    }
+}
