f9cb2ed2829c9ac77ab8f73bc0fa94e73968b3b3
Todd <teese.todd@gmail.com>
2021-03-19 20:16:40 +0000

    button to spin the wheel

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 console.log('App connected Bro!');
 
 const wheelHolder = document.querySelector('.wheelHolder');
+const outputText = document.querySelector('#outputText');
 const ctx = wheelHolder.getContext('2d');
 const options = {width: 600, height: 600, numberOfSegments: 10}; // allow user input on options
 
@@ -8,24 +9,34 @@ wheelHolder.width = options.width;
 wheelHolder.height = options.height;
 wheelHolder.style.border = 'solid 1px black';
 
+const minimumTimeFrame = 4000;
+let randomTimeFrame = ((Math.random() * 5) * 1000) + minimumTimeFrame;
+
+let offset = 0;
+let offsetDelta = 0.3;
+
+let start;
+let previousTimeStamp;
+
 function convertToRadians(degrees) {
     return (Math.PI/180) * degrees;
+}
+
+function spinWheel() {
+    console.log('Spin the Wheel!');
+    start = undefined;
+    previousTimeStamp = undefined;
+    randomTimeFrame = ((Math.random() * 5) * 1000) + minimumTimeFrame;
+    offset = 0;
+    window.requestAnimationFrame(step);
 }
 
 let formInput = document.querySelector('form');
 formInput.addEventListener('submit', event => {
     event.preventDefault();
     console.log('Form Event');
-
-    const segments = document.querySelector('#segmentInput').value;
-    console.log(segments);
-    if(segments) {
-        options.numberOfSegments = segments;
-        wheelObj.setNumberOfSegments(options.numberOfSegments);
-    }
 })
 
-let outputText = document.querySelector('#outputText');
 
 let wheelObj = new Wheel();
 wheelObj.setNumberOfSegments(options.numberOfSegments);
@@ -33,20 +44,8 @@ wheelObj.centerX = options.width / 2;
 wheelObj.centerY = options.height / 2;
 wheelObj.radius = options.width / 3;
 
-let offset = 0;
-let start;
-let previousTimeStamp;
-
 wheelObj.draw(ctx, offset);
 console.log({wheel: wheelObj});
-// outputText.innerText = wheelObj.getSelectedSegment(offset);
-
-// randomTimeFrame
-const minimumTimeFrame = 4000;
-let randomTimeFrame = ((Math.random() * 5) * 1000) + minimumTimeFrame;
-let offsetDelta = 0.3;
-
-
 
 function step(timestamp) {
 
@@ -64,9 +63,7 @@ function step(timestamp) {
 
     wheelObj.draw(ctx,offset);
 
-    outputText.innerText = wheelObj.getSelectedSegment(offset) + " " + offset;
-
-    console.log({elapsed: elapsed, randomTimeFrame : randomTimeFrame});
+    outputText.innerText = "Selected Option : " + wheelObj.getSelectedSegment(offset);
 
     offset += offsetDelta * delta;
     offset = offset < 360 ? offset :offset % 360;
@@ -77,6 +74,8 @@ function step(timestamp) {
 
     if(timeDelta < 3000) {
         offsetDelta = (timeDelta / 10000);
+    } else {
+        offsetDelta = 0.3; // default accel
     }
 
   
